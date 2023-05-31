50306fee7a3c1c976f98a188f24e7c48bf3be989
eduardonk9999 <thiago.novak7@gmail.com>
2023-05-31 15:40:42 -0300

    feat:interatividade

--- src/AnimaTelaDesenho.java
+++ src/AnimaTelaDesenho.java
@@ -3,13 +3,53 @@ import java.awt.*;
 
 public class AnimaTelaDesenho extends JFrame {
     private JPanel tela;
+    private int fps = 1000 / 20;
+    private int ct; //contador
+    private boolean anima = true;
+
+    public void iniciaAnimacao() {
+        long prxAtualizacao = 0;
+        while (anima) {
+            if(System.currentTimeMillis() >= prxAtualizacao) {
+                ct++;
+                tela.repaint();
+
+                prxAtualizacao = System.currentTimeMillis() + fps;
+
+                if(ct == 100) {
+                    anima = false;
+                }
+            }
+        }
+    }
 
     public AnimaTelaDesenho() {
         tela = new JPanel() {
+
             @Override
             public void paintComponent(Graphics g) {
-                g.setColor(Color.BLUE)
+                g.setColor(Color.BLUE);
+                g.drawLine(0, 240 + ct, 640, 240 + ct);
+                g.drawRect(10, 25 + ct, 20, 20);
+                g.drawOval(30 + ct, 20, 40, 30);
+                g.setColor(Color.YELLOW);
+                g.drawLine(320 - ct, 0, 320 - ct, 480);
+                g.fillRect(110, 125, 120 - ct, 120 - ct);
+                g.fillOval(230, 220, 240 + ct, 230);
+                g.setColor(Color.RED);
+                g.drawString("Eu seria um ótimo Score! " +
+                        ct, 5, 10);
             }
         };
+        getContentPane().add(tela);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize( 640, 480);
+        setVisible(true);
+        tela.repaint();
     }
+    public static void main(String[] args) {
+        AnimaTelaDesenho anima = new AnimaTelaDesenho();
+        anima.iniciaAnimacao();
+    }
+
 }
--- src/Interatividade.java
+++ src/Interatividade.java
@@ -1,8 +1,94 @@
-import javax.swing.*;
 
-public class Interatividade {
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Interatividade extends JFrame{
+
+
     private JPanel tela;
     private int px;
     private int py;
     private boolean jogando = true;
+
+    private final int FPS = 1000 / 20; // 50
+
+
+    public void inicia() {
+        long prxAtualizacao = 0;
+        while (jogando) {
+            if(System.currentTimeMillis() >= prxAtualizacao) {
+                this.tela.repaint();
+                prxAtualizacao = System.currentTimeMillis() + FPS;
+            }
+        }
+    }
+
+    public Interatividade() {
+        super.addKeyListener(new KeyListener() {
+            @Override
+            //Evento para tecla apertada
+            public void keyTyped(KeyEvent e) {
+
+            }
+            @Override
+            //Evento para tecla liberada
+            public void keyReleased(KeyEvent e) {
+
+            }
+
+            @Override
+            //Evento para tecla pressionada
+            public void keyPressed(KeyEvent e) {
+                int tecla = e.getKeyCode();
+                switch (tecla) {
+                    case KeyEvent.VK_ESCAPE:
+                        // Tecla ESC
+                        jogando = false;
+                       dispose(); // para fechar a janela
+                        break;
+                    case KeyEvent.VK_UP:
+                        // Seta para cima
+                        py--;
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        // Seta para baixo
+                        py++;
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        // Seta para esquerda
+                        px--;
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        // Seta para direita
+                        px++;
+                        break;
+                }
+            }
+        });
+        tela = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                g.setColor(Color.WHITE);
+                g.fillRect(0, 0,
+                        tela.getWidth(), tela.getHeight());
+                int x = tela.getWidth() / 2 - 20 + px;
+                int y = tela.getHeight() / 2 - 20 + py;
+                g.setColor(Color.BLUE);
+                g.fillRect(x, y, 40, 40);
+                g.drawString("Agora eu estou em "
+                        + x + "x" + y, 5, 10);
+            }
+        };
+        getContentPane().add(tela);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(640, 480);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        Interatividade jogo = new Interatividade();
+        jogo.inicia();
+    }
+
 }
